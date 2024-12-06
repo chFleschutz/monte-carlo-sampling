8cf7b4e6a0c7a1daa71cf2d146f6540c0971b354
Christoph Fleschutz <92266455+chFleschutz@users.noreply.github.com>
2024-12-06 17:11:44 +0100

    Add rejection sampling

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 #include <functional>
 #include <iostream>
 #include <random>
+#include <array>
+
+#include "scope_timer.h"
 
 class MonteCarlo
 {
@@ -12,15 +15,13 @@ public:
 	{
 	}
 
-	auto estimate(int samples, float min = 0.0f, float max = 1.0f) -> std::pair<float, float>
+	auto estimate(int samples) -> std::pair<float, float>
 	{
-		std::uniform_real_distribution<float> dis(min, max);
-
 		float sum = 0.0f;
 		float sumSquared = 0.0f;
 		for (int i = 0; i < samples; i++)
 		{
-			float x = dis(gen);
+			float x = rejectionSample();
 			float y = m_f(x) / m_pdf(x);
 			sum += y;
 			sumSquared += y * y;
@@ -32,35 +33,93 @@ public:
 	}
 
 private:
+	auto rejectionSample() -> float
+	{
+		float max = m_pdf(1.0f);
+		while (true)
+		{
+			float x = dis(gen);
+			float u = dis(gen);
+			if (u * max <= m_pdf(x))
+				return x;
+		}
+	}
+
 	std::function<float(float)> m_f;	// Function to integrate
 	std::function<float(float)> m_pdf;	// Probability density function
 
 	std::random_device rd;
 	std::mt19937 gen{ rd() };
+	std::uniform_real_distribution<float> dis{ 0.0f, 1.0f };
 };
 
-static void estimate(MonteCarlo& mc, int samples)
+template<size_t N>
+static void estimate(MonteCarlo& mc, std::array<int, N> sampleCounts)
 {
-	auto [mean, variance] = mc.estimate(samples);
-	std::cout << std::format("{:>10} Samples - Estimate: {:<10} Variance: {:<10}\n", samples, mean, variance);
+	ScopeTimer<std::chrono::milliseconds> timer;
+
+	for (auto samples : sampleCounts)
+	{
+		auto [mean, variance] = mc.estimate(samples);
+		std::cout << std::format("{:>10} Samples - Estimate: {:<10} Variance: {:<10}\n", samples, mean, variance);
+	}
 }
 
 auto main() -> int
 {
+	std::cout << "Monte Carlo Integration\n";
+	std::cout << "------------------------\n";
+
 	auto f = [](float x) -> float
 		{
 			return 5.0f * std::pow(x, 4);
 		};
 
-	auto pdf = [](float x) -> float
-		{
-			return 1.0f;
-		};
+	auto sampleCounts = std::array{ 100, 10'000, 1'000'000 };
 
-	MonteCarlo mc(f, pdf);
-	estimate(mc, 100);
-	estimate(mc, 10'000);
-	estimate(mc, 1'000'000);
+	std::cout << "\nConstant PDF\n";
+	{
+		auto constant_pdf = [](float x) -> float
+			{
+				return 1.0f;
+			};
+
+		MonteCarlo mc(f, constant_pdf);
+		estimate(mc, sampleCounts);
+	}
+
+	std::cout << "\nLinear PDF\n";
+	{
+		auto linear_pdf = [](float x) -> float
+			{
+				return 2.0f * x;
+			};
+
+		MonteCarlo mc(f, linear_pdf);
+		estimate(mc, sampleCounts);
+	}
+
+	std::cout << "\nQuadratic PDF\n";
+	{
+		auto quadratic_pdf = [](float x) -> float
+			{
+				return 3.0f * x * x;
+			};
+
+		MonteCarlo mc(f, quadratic_pdf);
+		estimate(mc, sampleCounts);
+	}
+
+	std::cout << "\nBad PDF\n";
+	{
+		auto bad_pdf = [](float x) -> float
+			{
+				return -2.0f * x + 2.0f;
+			};
+
+		MonteCarlo mc(f, bad_pdf);
+		estimate(mc, sampleCounts);
+	}
 
 	return 0;
 }
